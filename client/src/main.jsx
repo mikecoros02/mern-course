858e577f9d1bc14b1a6af140e5301f9249220951
Mike Coros <mike.coros02@gmail.com>
2024-09-05 13:58:10 +0800

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
+import { ToastContainer } from 'react-toastify';
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <ToastContainer position='top-center' />
+  </React.StrictMode>
+);
